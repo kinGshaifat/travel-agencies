9890a11a04e5d76dd85a3f0097e9419754b745cb
shaifat <skshaifatmurshed@gmail.com>
2018-02-22 23:38:44 +0530

    social media 1st

---
+++ signup.php
@@ -0,0 +1,138 @@
+
+<!DOCTYPE HTML>
+<!--
+	Road Trip by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+<head>
+<title>sign up</title>
+<!--meta tags -->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--//meta tags ends here-->
+<!--booststrap-->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" >
+ 
+<!--//booststrap end-->
+
+<!-- font-awesome icons -->
+   <link rel="stylesheet" href="css/font-awesome.min.css" />
+
+<!-- //font-awesome icons -->
+<!--stylesheets-->
+<link href="css/style.css" rel='stylesheet' type='text/css' media="all">
+<!--//style sheet end here-->
+<link rel="stylesheet" href="css/lightbox.css">
+<link href="//fonts.googleapis.com/css?family=Encode+Sans+Condensed:300,400,500,600,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Abel" rel="stylesheet">
+</head>
+	<body class="subpage">
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<div class="logo"><a href="index.php">Knowledge Management System</a></div>
+				<a href="#menu"><span>Menu</span></a>
+			</header>
+
+		<!-- Nav -->
+			
+
+		<!-- Content -->
+		<!--
+			Note: To show a background image, set the "data-bg" attribute below
+			to the full filename of your image. This is used in each section to set
+			the background image.
+		-->
+			<section id="post" class="wrapper bg-img" data-bg="t2.jpg">
+				<div class="inner">
+					<article class="box">
+						<header>
+							<h2 align="center" style="color:green"><mark><i>Create a new account </i></mark></h2>
+							
+						</header>
+						<form method="post" action=" " enctype="multipart/form-data">
+<table align="center" style="background-color:#666">
+<tr>
+	<td><b>First Name</b>*</td>
+	<td><input type='text' name="fname" placeholder='Enter your first name...' </td><br>
+</tr>
+<tr>
+<tr>
+	<td><b>Middle Name</b></td>
+	<td><input type='text' name="mname" placeholder='Enter your middle name...' </td><br>
+</tr>
+<tr>
+	<td><b>last Name</b>*</td>
+	<td><input type='text' name="lname" placeholder='Enter your last name...' </td><br>
+</tr>
+<tr>
+	<td><b>Email</b>*</td>
+	<td><input type='email' name="email" placeholder='Enter your Email Id...' </td><br>
+</tr>
+<tr>
+	<td><b>Password</b>*</td>
+	<td><input type="password" name="password" placeholder="Enter 6 character password"><br></td>
+</tr>
+<tr>
+	<td><b>Phone no.</b>*</td>
+	<td><input type="text" name="phone" placeholder="Enter your mobile number"></td>
+</tr>
+<tr>
+	<td><b>Gender</b>*</td>
+	<td><select name="gender">
+    <option value="">-- select one --</option>
+  <option value="Male" style="color:black">Male</option>
+  <option value="Female" style="color:black">Female</option>
+  <option value="other" style="color:black">other</option>
+  </option>
+ </select></td>
+</tr>
+<tr>
+	<td><b>Date of birth*</b></td>
+	<td><input type="date" name="dob"></td>
+</tr>
+<tr>
+	<td><b>About</b>*</td>
+	<td><input type='text' name="about" placeholder='write something ...' </td><br>
+</tr>
+<tr>
+	<td><b>Occupation</b></td>
+	<td><input type="text" name="occupation"></td>
+</tr>
+<tr>
+	<td><b>Address</b>*</td>
+	<td><textarea name="address" name="address" placeholder="Enter your address"></textarea></td><br>
+</tr>
+<tr>
+	<td><b>Choose a pic</b>*</td>
+	<td><input type="file" name="file"></td>
+</tr>
+<tr>
+	<td><b>Security question</b>*</td>
+    <td><select name="securityquestion">
+    <option value="">-- select one --</option>
+  <option value="nickname" style="color:black">My nick name</option>
+  <option value="petname" style="color:black">My pet name</option>
+  <option value="hobby" style="color:black">My hobby</option>
+  </option>
+ </select></td>
+	</tr>
+<tr>
+	<td><b>Security Answer</b>*</td>
+	<td><input type='text' name="securityanswer" placeholder='Enter your Answer...' ></td><br>
+</tr>
+<tr>
+	<td><input type='submit' name='ok' value='Sign up'></td>
+</tr>
+
+</table>
+</form>
+<p align="center" style="color:red"><b>N/A- "The * mark area should be fulfilled by new user"</b></p><br>
+
+	</body>
+</html>
